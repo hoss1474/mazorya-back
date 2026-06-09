efaedf7b15e1d122f92a411d5af0fee8c123f33d
hossein hosseini <121756136+hoss1474@users.noreply.github.com>
2026-06-09 19:39:29 +0330

    update

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,13 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    'allowed_origins' => [
+        'https://cardifygroup.com',
+        'https://www.cardifygroup.com',
+        'https://www.api.cardifygroup.com',
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+    ],
 
     'allowed_origins_patterns' => [],
 
